4894ec4d74b573401040de848d459281cf0b9c44
KrishnaTeja2121 <37261298+KrishnaTeja2121@users.noreply.github.com>
2024-08-28 12:03:25 +0530

    #2

--- src/test/java/Ranish/AutomationFrameworkPractice/test.java
+++ src/test/java/Ranish/AutomationFrameworkPractice/test.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -17,15 +20,32 @@ public class test {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		String productName="ZARA COAT 3";
+		
 		driver.get("https://rahulshettyacademy.com/client");
 		driver.findElement(By.id("userEmail")).sendKeys("test21@test.com");		
 		driver.findElement(By.id("userPassword")).sendKeys("Test1234");		
 		driver.findElement(By.id("login")).click();
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
+		
 		List<WebElement> products=driver.findElements(By.cssSelector(".mb-3"));
 		WebElement prod=products.stream().filter(
-				product->product.findElement(By.cssSelector("b")).getText().equals("ZARA COAT 3")).findFirst().orElse(null);
+				product->product.findElement(By.cssSelector("b")).getText().equals(productName)).findFirst().orElse(null);
 		
 		prod.findElement(By.cssSelector(".card-body button:last-of-type")).click();
+		
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
+		wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+		//wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".ng-animating")));
+		driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+		
+		List<WebElement> cartProducts=driver.findElements(By.cssSelector(".cartSection h3"));
+		Boolean match=cartProducts.stream().anyMatch(cartProduct->cartProduct.getText().equalsIgnoreCase(productName));
+		Assert.assertTrue(match);
+		
+		driver.findElement(By.cssSelector(".totalRow button")).click();
 		
 
 	}
